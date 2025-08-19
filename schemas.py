7becfe50c0b9c587e39b8a35042cf4fefdf49ef4
fladischer-tristan <fladischertristan@gmail.com>
2025-08-19 16:22:04 +0200

    Added some schemas that add some OOP to the engine. Added the parent class ´Piece´ and child classes such as ´Pawn´, some Enums for readability.

---
+++ schemas.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+from enum import Enum
+from pydantic import BaseModel
+from abc import ABC, abstractmethod
+
+
+# Enum for chess pieces
+class ChessPieceType(Enum):
+    PAWN = "P"
+    BISHOP = "B"
+    KNIGHT = "N"
+    ROOK = "R"
+    QUEEN = "Q"
+    KING = "K"
+
+
+# Enum for chess colors black and white
+class ChessColor(Enum):
+    BLACK = 0
+    WHITE = 1
+
+
+# Class for representing a board position
+class Coordinate(BaseModel):
+    x: int
+    y: int
+
+
+
+# Parent class
+class Piece:
+    def __init__(self, piece_type: ChessPieceType):
+        self.piece_type = piece_type
+        
+
+    # Interface for children classes, since each child moves differently (Pawns, rooks, etc.)
+    @abstractmethod
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+     
+# Child Classes
+class Pawn(Piece):
+    def __init__(self, color: ChessColor):
+        super().__init__(piece_type = ChessPieceType.PAWN)
+
+        self.color = color
+        self.piece_value = 100 # TODO ADD CONSTANTS TABLE FOR VALUES
+
+
+    # Moving from x1,y1 to x2,y2 on the board
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+class Bishop(Piece):
+    def __init__(self, color: ChessColor):
+        super().__init__(piece_type = ChessPieceType.BISHOP)
+
+        self.color = color
+        self.piece_value = 300 # TODO ADD CONSTANTS TABLE FOR VALUES
+
+
+    # Moving from x1,y1 to x2,y2 on the board
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+class Knight(Piece):
+    def __init__(self, color: ChessColor):
+        super().__init__(piece_type = ChessPieceType.KNIGHT)
+
+        self.color = color
+        self.piece_value = 300 # TODO ADD CONSTANTS TABLE FOR VALUES
+
+
+    # Moving from x1,y1 to x2,y2 on the board
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+class Rook(Piece):
+    def __init__(self, color: ChessColor):
+        super().__init__(piece_type = ChessPieceType.ROOK)
+
+        self.color = color
+        self.piece_value = 500 # TODO ADD CONSTANTS TABLE FOR VALUES
+
+
+    # Moving from x1,y1 to x2,y2 on the board
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+
+class Queen(Piece):
+    def __init__(self, color: ChessColor):
+        super().__init__(piece_type = ChessPieceType.PAWN)
+
+        self.color = color
+        self.piece_value = 900 # TODO ADD CONSTANTS TABLE FOR VALUES
+
+
+    # Moving from x1,y1 to x2,y2 on the board
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+class King(Piece):
+    def __init__(self, color: ChessColor):
+        super().__init__(piece_type = ChessPieceType.PAWN)
+
+        self.color = color
+        self.piece_value = 5000 # TODO ADD CONSTANTS TABLE FOR VALUES
+
+
+    # Moving from x1,y1 to x2,y2 on the board
+    def move(origin: Coordinate, target: Coordinate, board):
+        pass
+
+
+
+
+
